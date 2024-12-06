74d65f8b9b829262cb2049fa7394fb3f326904f9
SyedFathir <syed.fathir@red-gate.com>
2024-12-06 18:07:40 +0000

    refactor test

--- AdventOfCode24/Day1/Day1Test.cs
+++ AdventOfCode24/Day1/Day1Test.cs
@@ -17,8 +17,8 @@ public class Day1Test
     public void Part2()
     {
         var input = File.ReadAllText("day1.txt");
-        var distance = new Day1().GetSimilarityScore(input);
+        var similarityScore = new Day1().GetSimilarityScore(input);
         
-        Console.WriteLine(distance);
+        Console.WriteLine(similarityScore);
     }
 }
